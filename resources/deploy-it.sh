70ce839399edfef27d5921895e9e731f421fb9e3
kordano <konrad.kuehne@rocketmail.com>
2015-04-05 16:11:25 +0200

    add port interaction

--- resources/deploy-it.sh
+++ resources/deploy-it.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop ceres-geschichte;
 docker rm ceres-geschichte;
-docker run -d -v /opt/data/ceres-geschichte/k-geschichte:/opt/data -v /opt/data/ceres-geschichte/k-log:/opt/log -v /opt/data/ceres-geschichte/k-benchmark:/opt/benchmark --name ceres-geschichte kordano/ceres-geschichte
+docker run -d -v /opt/data/ceres-geschichte/k-geschichte:/opt/data -v /opt/data/ceres-geschichte/k-log:/opt/log -v /opt/data/ceres-geschichte/k-benchmark:/opt/benchmark  -p 31744:31744 --name ceres-geschichte kordano/ceres-geschichte
